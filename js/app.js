710f342e15104f1944ba99590bd22c16a74b5d77
Obnostic <unklefoddy@gmail.com>
2025-01-19 23:30:51 -0500

    Almost got it working - hung up at Restart

--- js/app.js
+++ js/app.js
@@ -1,154 +1,103 @@
-/*  Before you get started, think about the win and lose conditions of the game.
-
-`Win Condition`: Player guesses the correct number within 3 tries.
-
-`Lose Condition`: Player guesses incorrectly 3 times.
-
-All elements that will be referenced in `app.js` will have an id. Read through `index.html` and take no
-te of which elements you will be referencing.
-
-If you get stuck, write psuedocode and break the game's functionality and logic into smaller pieces.
-
-- [ ] The span with the id of `guess-message` should output whether the guess is too high, too low, if the player won, or if the player lost.
-- [ ] The span with the id of `current-guess` should output the guess that was just submitted.
-- [ ] The span with the id of `computer-guess` should output the guess that was randomly generated.
-- [ ] The span with the id of `guess-history` should output your guess history.
-- [ ] The "Submit Guess" button should be enabled at the beginning of the game, but when the game is over
- it should be disabled.
-- [ ] The "Restart" button should be disabled at the beginning of the game, but when the
- game is over it should be enabled.
-- [ ] When the "Restart" button is clicked, all values and text output should reset.
-
-*/
-// DECLARATION OF INTERNAL VARIABLES AND THEIR LINKS TO FIELD IDs IN THE WEB PAGE
-
-
-const submissionBtn = document.getElementById("submit-btn");
-
-const restartBtn = document.getElementById("restart-btn");
-let guessMessage = document.getElementById("guess-message");
-let currentGuess = document.getElementById("current-guess");
-
-let sButtonClicked = false;
-let turn = 0
-
-
-/* Event delegation
-Inside a list only?    <div class="btn-container">
-                    <button id="submit-btn" class="btn">Submit Guess</button>
-                    <button id="restart-btn" class="btn" disabled>
-                        Restart
-                    </button>
-                </div>
-
-
- } )
-
-TURNING THE BUTTONS ON AND OFF (CHECK OPACITY SWITCHING)
-document.getElementById("submit-btn").disabled = true;       //OFF
-document.getElementById("submit-btn").disabled = false;      //ON
-
-
-document.getElementById("restart-btn").disabled = true;       //OFF
-document.getElementById("restart-btn").disabled = false;      //ON
-
-
- corresponding to HTML code -  restart-btn is declared in a disabled state by: <button .... disabled> Visible Button Label </button> 
- 
-
-/* This is the HTML code for the buttons
-<div class="btn-container">
-//<button id="submit-btn" class="btn">Submit Guess</button>
-<button id="restart-btn" class="btn" disabled>
-    Restart
-</button>
-</div>
-
-This is the CSS for the Restart button's beginning state. 
-When Submit Guess is pushed it will run through the game
-At the end of the game or when the guess is correct (breakout condition),
-the Submit Guess button is disabled and the Restart button has full view.
-
-
-*/
-
-
-
+// Number Guessing Game
 
 "use strict";
 
-/*
-const submit = document.getElementById("submit-btn");  Comm with HTML buttons
-const restart = document.getElementById("restart-btn");
-*/
+// DECLARATION OF INTERNAL VARIABLES AND THEIR LINKS TO FIELD IDs IN THE WEB PAGE
 
-//  Randomization
+const submissionBtn = document.getElementById("submit-btn");
+const guessInput = document.getElementById("guess-input");
+const restartBtn = document.getElementById("restart-btn");
+const guessMessage = document.getElementById("guess-message");
+const currentGuess = document.getElementById("current-guess");
+const guessHistory = document.getElementById("guess-history")
+const computerGuess = document.getElementById("computer-guess");
 
-
-
-let targetNumber = Math.floor(Math.random()*10+1);
-console.log(`The secret number is ${targetNumber}`);
-
-
-
-
-
-
-
-/*  Button activators
-
-submit.addEventListener("click", function () {
-
-} 
-
-restart.addEventListener("click", function () {
-
-} 
-*/
+let sButtonClicked = false;
+let rButtonClicked = true;
+let turn=0;
+let targetNumber=0;
+let secret=0;
+let userGuess=0;
+let guessHistoryString="";
 
 
-function theMainGame(userGuess) {
 
-while (turn <3) {
-
-    console.log("User Guess: " + userGuess);
-    console.log("Computer guess: " + targetNumber);
-    console.log("This is Turn "+turn);
-    //sButtonClicked = !sButtonClicked;
+function theMainGame() {
+    console.log("Going into the main game");
+    console.log("This is Turn "+ turn);
     if (userGuess == targetNumber) {
-        console.log ("Success!");
+        guessMessage.textContent = "Success!";
+        computerGuess.textContent = targetNumber;
         document.getElementById("submit-btn").disabled = true;
         document.getElementById("restart-btn").disabled = false; 
-        break;
-        } else {console.log("Wrong! Try Again.");
-            turn++;
-                submissionBtn.addEventListener("click", function() {
-                if (sButtonClicked) {
-                    let userGuess = document.getElementById("guess-input").value;
-                }
-            })
+    } else {
+        if (turn < 3) {
+          guessMessage.textContent = "Wrong! Try Again.";  // display in the 'Guess History' field  
+             } else {                                   // WHY DOES THIS NOT BLOODY WORK
+               guessMessage.textContent ="Sorry pal, you lost!";
+               computerGuess.textContent = targetNumber;
+               document.getElementById("submit-btn").disabled = true;
+               document.getElementById("restart-btn").disabled = false; 
+             }                    
+        document.getElementById("submit-btn").disabled = false;
+        document.getElementById("restart-btn").disabled = true;
+        sButtonClicked = !sButtonClicked;     
     }
 }
+
+
+
+function grabTheStuffAndPlay()  {
+  if (turn<4)   {
+    userGuess=0
+    submissionBtn.addEventListener("click", function() {
+      sButtonClicked = !sButtonClicked;
+      console.log(`button is now ${sButtonClicked}`);
+      if (sButtonClicked) {
+        userGuess = guessInput.value;                   // Grab the number the user put in the window
+        currentGuess.textContent = userGuess;           // display in the 'Your Guess' field
+        guessHistoryString = (guessHistoryString + userGuess.toString()+"  ");   //  Add guess to prior history (if any)
+        guessHistory.textContent = guessHistoryString;  // display in the 'Guess History' field  
+        turn++;
+        theMainGame();
+        }
+      })
+  }
 }
 
-//if (turn=3) {console.log("Sorry pal, you lost!");
-//document.getElementById("submit-btn").disabled = true;
-//document.getElementById("restart-btn").disabled = false;
-//}
 
-
-  
-
+function generateRandomNumber() {
+targetNumber = Math.floor(Math.random()*10+1);          // PICK THE RANDOM NUMBER YOU SILLY
+console.log(`The secret number is ${targetNumber}`);    // HIDE THE SECRET FROM THE USER (TEMP USE)
+}
 
 
 
+function startTheGame(turn) {
+    generateRandomNumber();
+    guessMessage.textContent = ""
 
-submissionBtn.addEventListener("click", function() {
-    sButtonClicked = !sButtonClicked;
-    if (sButtonClicked) {
-        let guessInputBox = document.getElementById("guess-input").value;
-           // document.getElementById(currentGuess).value = guessInputBox;
-            
-            theMainGame(guessInputBox);};
-        })
+    if (turn < 3) { grabTheStuffAndPlay(); }
+    else {
+    document.getElementById("submit-btn").disabled = true;
+    document.getElementById("restart-btn").disabled = false;
+    }
+}
+
+
+
+startTheGame(turn);
+
+
     
+
+
+
+
+
+
+
+
+
+
+   
